225efd234bed861d2f3efd685f04a7b3dec3c918
olim7t <omichallat+github@gmail.com>
2015-06-25 10:01:48 +0200

    Simplify RetryDecisionTest.
    Remove unneeded nested class.

--- driver-core/src/test/java/com/datastax/driver/core/policies/RetryDecisionTest.java
+++ driver-core/src/test/java/com/datastax/driver/core/policies/RetryDecisionTest.java
@@ -17,48 +17,49 @@ package com.datastax.driver.core.policies;
 
 import org.testng.annotations.Test;
 
-import static org.testng.Assert.assertEquals;
+import static org.assertj.core.api.Assertions.assertThat;
 
-import com.datastax.driver.core.ConsistencyLevel;
-import com.datastax.driver.core.Statement;
-import com.datastax.driver.core.WriteType;
+import com.datastax.driver.core.policies.RetryPolicy.RetryDecision;
+
+import static com.datastax.driver.core.ConsistencyLevel.ONE;
+import static com.datastax.driver.core.policies.RetryPolicy.RetryDecision.Type.IGNORE;
+import static com.datastax.driver.core.policies.RetryPolicy.RetryDecision.Type.RETHROW;
+import static com.datastax.driver.core.policies.RetryPolicy.RetryDecision.Type.RETRY;
 
 public class RetryDecisionTest {
 
-    /**
-     * Test RetryPolicy to ensure the basic unit get tests for the RetryDecisions.
-     */
-    public static class TestRetryPolicy implements RetryPolicy {
-        public RetryDecision onReadTimeout(Statement query, ConsistencyLevel cl, int requiredResponses, int receivedResponses, boolean dataRetrieved, int nbRetry) {
-            return RetryDecision.rethrow();
-        }
-
-        public RetryDecision onWriteTimeout(Statement query, ConsistencyLevel cl, WriteType writeType, int requiredAcks, int receivedAcks, int nbRetry) {
-            return RetryDecision.rethrow();
-        }
-
-        public RetryDecision onUnavailable(Statement query, ConsistencyLevel cl, int requiredReplica, int aliveReplica, int nbRetry) {
-            return RetryDecision.rethrow();
-        }
-
-        public static void testRetryDecision() {
-            assertEquals(RetryDecision.retry(ConsistencyLevel.ONE).getType(), RetryDecision.Type.RETRY);
-            assertEquals(RetryDecision.retry(ConsistencyLevel.ONE).getRetryConsistencyLevel(), ConsistencyLevel.ONE);
-            assertEquals(RetryDecision.rethrow().getType(), RetryDecision.Type.RETHROW);
-            assertEquals(RetryDecision.ignore().getType(), RetryDecision.Type.IGNORE);
-
-            assertEquals(RetryDecision.retry(ConsistencyLevel.ONE).toString(), "Retry at " + ConsistencyLevel.ONE + " on same host.");
-            assertEquals(RetryDecision.tryNextHost(ConsistencyLevel.ONE).toString(), "Retry at " + ConsistencyLevel.ONE + " on next host.");
-            assertEquals(RetryDecision.rethrow().toString(), "Rethrow");
-            assertEquals(RetryDecision.ignore().toString(), "Ignore");
-        }
-    }
-
-    /**
-     * Test RetryDecision get variables and defaults are correct.
-     */
     @Test(groups = "unit")
-    public void RetryDecisionTest() throws Throwable {
-        TestRetryPolicy.testRetryDecision();
+    public void should_expose_decision_properties() throws Throwable {
+        RetryDecision retryAtOne = RetryDecision.retry(ONE);
+        assertThat(retryAtOne.getType())
+            .isEqualTo(RETRY);
+        assertThat(retryAtOne.getRetryConsistencyLevel())
+            .isEqualTo(ONE);
+        assertThat(retryAtOne.isRetryCurrent())
+            .isTrue();
+        assertThat(retryAtOne.toString())
+            .isEqualTo("Retry at ONE on same host.");
+
+        RetryDecision tryNextAtOne = RetryDecision.tryNextHost(ONE);
+        assertThat(tryNextAtOne.getType())
+            .isEqualTo(RETRY);
+        assertThat(tryNextAtOne.getRetryConsistencyLevel())
+            .isEqualTo(ONE);
+        assertThat(tryNextAtOne.isRetryCurrent())
+            .isFalse();
+        assertThat(tryNextAtOne.toString())
+            .isEqualTo("Retry at ONE on next host.");
+
+        RetryDecision rethrow = RetryDecision.rethrow();
+        assertThat(rethrow.getType())
+            .isEqualTo(RETHROW);
+        assertThat(rethrow.toString())
+            .isEqualTo("Rethrow");
+
+        RetryDecision ignore = RetryDecision.ignore();
+        assertThat(ignore.getType())
+            .isEqualTo(IGNORE);
+        assertThat(ignore.toString())
+            .isEqualTo("Ignore");
     }
 }
